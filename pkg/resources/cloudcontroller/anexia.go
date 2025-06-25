0534cfe345618b30efdadf5f77f2a519cf6904a3
Kim Fehrs <kfehrs@anexia-it.com>
2025-06-25 14:44:54 +0200

    apply update ccm patch

--- pkg/resources/cloudcontroller/anexia.go
+++ pkg/resources/cloudcontroller/anexia.go
@@ -35,7 +35,7 @@ import (
 
 const (
 	AnexiaCCMDeploymentName = "anx-cloud-controller-manager"
-	anexiaCCMVersion        = "1.5.6"
+	anexiaCCMVersion        = "1.5.9"
 )
 
 func anexiaDeploymentReconciler(data *resources.TemplateData) reconciling.NamedDeploymentReconcilerFactory {
